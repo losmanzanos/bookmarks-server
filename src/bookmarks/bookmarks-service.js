ae9453fb1ad0a262f378dddaa55ac07e84d382cb
Chad Moravec <chad@pop-os.localdomain>
2021-01-10 17:43:45 -0700

    validation

--- src/bookmarks/bookmarks-service.js
+++ src/bookmarks/bookmarks-service.js
@@ -2,6 +2,9 @@ const BookmarksService = {
   getAllBookmarks(knex) {
     return knex.select("*").from("bookmarks");
   },
+  getById(knex, id) {
+    return knex.from("bookmarks").select("*").where("id", id).first();
+  },
   insertBookmark(knex, newBookmark) {
     return knex
       .insert(newBookmark)
@@ -10,9 +13,6 @@ const BookmarksService = {
       .then((rows) => {
         return rows[0];
       });
-  },
-  getById(knex, id) {
-    return knex.from("bookmarks").select("*").where("id", id).first();
   },
   deleteBookmark(knex, id) {
     return knex("bookmarks").where({ id }).delete();
